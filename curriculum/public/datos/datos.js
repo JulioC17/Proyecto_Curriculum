3fa65ecb4549c6f0884729cd49b009803a938532
JulioC17 <159253136+JulioC17@users.noreply.github.com>
2024-08-07 16:13:30 +0200

    datos trminados

---
+++ curriculum/public/datos/datos.js
@@ -0,0 +1,72 @@
+const data = {
+    name: "Julio Cesar",
+    telefono: "123-456-789",
+    GitHub: "https://github.com/JulioC17",
+    address: "Ayuntamiento de Valencia",
+    email: "juliocesarsantosreyes@gmail.com",
+    avatar:"https://www.shutterstock.com/image-vector/young-smiling-man-avatar-brown-600nw-2261401207.jpg",
+    aboutMe:"Soy una persona empática con la capacidad de trabajar de manera eficaz tanto en equipo como de forma individual. Mi pasión por la programación y mi afición por los retos me han impulsado a desarrollar habilidades en diversas áreas tecnológicas, incluyendo el trading. Además de mi interés profesional, soy un entusiasta de los deportes, especialmente del fútbol, lo cual me ha enseñado la importancia de la disciplina y el trabajo en equipo. Me considero un aprendiz rápido, siempre dispuesto a adquirir nuevos conocimientos y habilidades que puedan contribuir al éxito",
+    profesion: "Desarrollador Full-Stack",
+    profesionAvatar:"https://img.freepik.com/fotos-premium/fondo-programacion-fotos-persona-que-trabaja-codigo-hacker_960020-209.jpg",
+    skills: [
+       {
+        tecnologia: "html5",
+        descripcion: "HTML5 es la quinta revisión importante del Lenguaje de Marcado de Hipertexto (HTML), que se utiliza para estructurar y presentar contenido en la World Wide Web. Publicado en octubre de 2014 por el World Wide Web Consortium (W3C), HTML5 introduce una serie de nuevas características y mejoras en comparación con sus predecesores, con el objetivo de mejorar la experiencia del usuario, facilitar el desarrollo web y soportar mejor los dispositivos móviles.",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png"
+       },
+       {
+        tecnologia: "css3",
+        descripcion: "CSS3 es la tercera versión del lenguaje de Hojas de Estilo en Cascada (Cascading Style Sheets), utilizado para describir la presentación y el diseño de documentos HTML o XML. CSS3 introduce nuevas características y mejoras respecto a las versiones anteriores, con el objetivo de proporcionar más herramientas y capacidades para los diseñadores web y desarrolladores, permitiendo la creación de interfaces de usuario más sofisticadas y atractivas.",
+        logo: "https://cdn.freebiesupply.com/logos/large/2x/css3-logo-png-transparent.png"
+       },{
+        tecnologia: "javascript",
+        descripcion: "JavaScript es un lenguaje de programación interpretado, de alto nivel y basado en prototipos, que se utiliza principalmente para crear y controlar contenido dinámico en páginas web. Fue desarrollado originalmente por Brendan Eich en 1995 y es parte fundamental del desarrollo web moderno, junto con HTML y CSS. JavaScript permite a los desarrolladores implementar características interactivas como animaciones, manejo de formularios, actualización de contenido en tiempo real, juegos, y mucho más.",
+        logo: "https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png"
+       },{
+        tecnologia: "react",
+        descripcion: "React es una biblioteca de JavaScript de código abierto, desarrollada por Facebook, que se utiliza para construir interfaces de usuario (UI) dinámicas y componentes de interfaz de usuario en aplicaciones web. Introducida por primera vez en 2013, React se ha convertido en una de las bibliotecas más populares para el desarrollo frontend debido a su eficiencia, flexibilidad y simplicidad.",
+        logo: "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+       },
+    ],
+    Experiencia: [
+        {
+            Posicion: "Desarrollador Back-end",
+            Compañía: "Apple Inc",
+            Inicio: "2018",
+            Descripcion: "Era el responsable de la lógica del servidor, la gestión de bases de datos y la integración de la aplicación con sistemas y servicios externos."
+        },
+        {
+            Posicion: "Desarrollador Full-Stack",
+            Compañía: "Microsoft S.A.",
+            Inicio: "2020",
+            Descripcion: "Me encargaba de la creación de interfaces de usuario y la lógica del servidor, así como de la gestión de bases de datos y la integración con servicios externos, cubriendo todos los aspectos del desarrollo web."
+        },{
+            Posicion: "Desarrollador Front-end",
+            Compañía: "Samsung Corporation",
+            Inicio: "2023",
+            Descripcion: "Me centraba en la creación y optimización de la interfaz de usuario y la experiencia visual de una aplicación web. Trabaja con tecnologías como HTML, CSS y JavaScript para implementar diseños y funcionalidades que sean atractivos."
+        },
+    ],
+    Educacion: "Me gradué en una de las Escuelas mas prestigiosas de España, The Powe MBA. En la promoción del año 2017 obtengo los honores del Mejor Desarrollador web y termino mis estudios con el Titulo de Desarrollador Full-Stack. Tambien durante mis estudios participé en muchos cursos optativos y con cierto grado de dificultad para los estudiantes mas arriesgados como el de Fundamentos Avanzados del Desarrollo web, Estructura de Datos y Algortimos y Diseño de Interfaces para usuarios",
+    Proyectos: [
+        {
+            nombre:"Proyecto Web Asincrono",
+            descripcion: "En este proyecto me dedique a hacer una landing page asincrona, que utilizaba los datos de una Api para mostrarle al usuario imagenes en base a los criterios de su busqueda mediante un input y algunas otras opciones de busqueda",
+            url: "https://github.com/JulioC17/Pyecto_Web_Asincrono.git",
+            imagen: "https://www.shutterstock.com/image-vector/flat-tiny-democracy-vector-illustration-600nw-1808874106.jpg"
+        },
+        {
+            nombre:"Proyecto DOM",
+            descripcion: "Aqui desarrolle y practique todo con respecto a la implementacion de DOM en una pagina web, la creacion, edicion, manipulacion y eliminacion de datos dinamicamnte en una pagina web con el uso de HTML5, CSS3 y JavaScript",
+            url: "https://github.com/JulioC17/Proyecto_DOM.git",
+            imagen: "https://a.ilovecoding.org/thumb/dom.png"
+        },{
+            nombre:"Proyecto Random Restaurante",
+            descripcion: "En este proyecto quise darle una solucion un poco creativa y comica al problema que todos hemos tenido alguna vez de no saber a que restaurante ir a comer, por lo que mediante el DOM, JS, HTML5, y CSS3 he creado una pagina donde el usuario puede introducir todas las opciones que tiene para ir a comer y esta le dara un resultado aleatorio de a donde salir",
+            url: "https://github.com/JulioC17/Random-Restaurant.git",
+            imagen: "https://img.freepik.com/vector-gratis/fachada-restaurante-estilo-dibujos-animados_23-2147539527.jpg"
+        },
+    ]
+
+
+}
